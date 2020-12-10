5636e29a9f6a7b07c4f1075222ed455094e9b99b
Cecep Rafqi al husni <CecepRafqi19@gmail.com>
2020-12-10 17:17:11 +0700

    uts

---
+++ app/src/main/java/com/example/resepmakanan/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.resepmakanan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public EditText username,password;
+    public Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.btnLogin){
+
+            String ambilUsername = username.getText().toString().trim();
+            String ambilPassword = password.getText().toString().trim();
+
+            if(ambilUsername.equals("cecep") && ambilPassword.equals("cecep")){
+                Intent intent = new Intent(MainActivity.this, JenisMakanan.class);
+                startActivity(intent);
+            }else {
+                Toast.makeText(this, "Username atau Password anda salah", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
